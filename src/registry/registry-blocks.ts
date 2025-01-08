3e1a3eae156f37d4a691c6834a28d34337e4e86e
meglerhagen <Christer.hagen@gmail.com>
2025-01-08 15:43:08 +0100

    before push

--- src/registry/registry-blocks.ts
+++ src/registry/registry-blocks.ts
@@ -2,6 +2,62 @@ import { type Registry } from "./schema";
 
     export const blocks = [
 {
+  "name": "action-button-template",
+  "type": "registry:block",
+  "title": "Action Button Template",
+  "description": "A template for action-button-template",
+  "files": [
+    {
+      "path": "action-button-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "action-button-template/app/layout.tsx",
+      "type": "registry:page",
+      "target": "app/layout.tsx"
+    },
+    {
+      "path": "action-button-template/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    },
+    {
+      "path": "action-button-template/app/component/action-button.tsx",
+      "type": "registry:page",
+      "target": "app/component/action-button.tsx"
+    }
+  ]
+},
+{
+  "name": "display-cards-template",
+  "type": "registry:block",
+  "title": "Display Cards Template",
+  "description": "A template for display-cards-template",
+  "files": [
+    {
+      "path": "display-cards-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "display-cards-template/app/layout.tsx",
+      "type": "registry:page",
+      "target": "app/layout.tsx"
+    },
+    {
+      "path": "display-cards-template/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    },
+    {
+      "path": "display-cards-template/app/component/display-cards.tsx",
+      "type": "registry:page",
+      "target": "app/component/display-cards.tsx"
+    }
+  ]
+},
+{
   "name": "expandable-card-template",
   "type": "registry:block",
   "title": "Expandable Card Template",
@@ -63,6 +119,67 @@ import { type Registry } from "./schema";
   ]
 },
 {
+  "name": "hero-badge-template",
+  "type": "registry:block",
+  "title": "Hero Badge Template",
+  "description": "A template for hero-badge-template",
+  "files": [
+    {
+      "path": "hero-badge-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "hero-badge-template/app/layout.tsx",
+      "type": "registry:page",
+      "target": "app/layout.tsx"
+    },
+    {
+      "path": "hero-badge-template/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    },
+    {
+      "path": "hero-badge-template/app/component/hero-badge.tsx",
+      "type": "registry:page",
+      "target": "app/component/hero-badge.tsx"
+    }
+  ]
+},
+{
+  "name": "hero-template",
+  "type": "registry:block",
+  "title": "Hero Template",
+  "description": "A template for hero-template",
+  "files": [
+    {
+      "path": "hero-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "hero-template/app/layout.tsx",
+      "type": "registry:page",
+      "target": "app/layout.tsx"
+    },
+    {
+      "path": "hero-template/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    },
+    {
+      "path": "hero-template/app/component/expandable-card.tsx",
+      "type": "registry:page",
+      "target": "app/component/expandable-card.tsx"
+    },
+    {
+      "path": "hero-template/app/component/hero.tsx",
+      "type": "registry:page",
+      "target": "app/component/hero.tsx"
+    }
+  ]
+},
+{
   "name": "landing-page-test",
   "type": "registry:block",
   "title": "Landing Page Test",
@@ -80,6 +197,52 @@ import { type Registry } from "./schema";
     },
     {
       "path": "landing-page-test/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    }
+  ]
+},
+{
+  "name": "popover-template",
+  "type": "registry:block",
+  "title": "Popover Template",
+  "description": "A template for popover-template",
+  "files": [
+    {
+      "path": "popover-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "popover-template/app/layout.tsx",
+      "type": "registry:page",
+      "target": "app/layout.tsx"
+    },
+    {
+      "path": "popover-template/app/globals.css",
+      "type": "registry:page",
+      "target": "app/globals.css"
+    },
+    {
+      "path": "popover-template/app/component/popover.tsx",
+      "type": "registry:page",
+      "target": "app/component/popover.tsx"
+    }
+  ]
+},
+{
+  "name": "tweet-card-template",
+  "type": "registry:block",
+  "title": "Tweet Card Template",
+  "description": "A template for tweet-card-template",
+  "files": [
+    {
+      "path": "tweet-card-template/app/page.tsx",
+      "type": "registry:page",
+      "target": "app/page.tsx"
+    },
+    {
+      "path": "tweet-card-template/app/globals.css",
       "type": "registry:page",
       "target": "app/globals.css"
     }
